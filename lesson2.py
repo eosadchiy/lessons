b1a1629ffba88096dd7760b9000f9a5a4884de0c
eosadchiy <e.osadchiy@smartweb.com.ua>
2018-05-31 18:45:18 +0300

    my_first_commit

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+x = -100
+
+if  x > 10:
+    print(x)
+elif x < 5 and x > 0:
+    if x + 11 > 13:
+        print('stalo bolshe 13')
+    else:
+        print(x)
+else:
+    if x - 100 > -200 and x - 100 < -50:
+        print('-200 -50 ' + str(x))
+    else:
+        print(str(x) + ' chto to poshlo na pravo')
